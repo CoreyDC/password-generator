2c3be90de3f92c32138e43656d58b5fb7dc3fabc
Corey Calbreath <corey_calbreath@yahoo.com>
2022-12-16 18:19:58 -0600

    added pseudocode to project

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,31 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
-}
-
-// Add event listener to generate button
+} 
+ 
 generateBtn.addEventListener("click", writePassword);
+
+// Assignment Code || ln 1
+
+// Write password to the #password input || ln 3-9
+
+// Add event listener to generate button || ln 11
+
+// Create generatePassword() function
+
+// Confirm do you want to include numbers?
+
+// Confirm do you want to include Letters?
+
+// Confirm do you want to include special characters?
+
+// Confirm do you want lower and uppercase letters? 
+
+// Prompt how many characters between 8 and 128 
+
+// Generate password
