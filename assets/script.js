e08314190d8f8121f27f219d2affffa1e73136d9
Corey Calbreath <corey_calbreath@yahoo.com>
2022-12-16 18:30:37 -0600

    addded first part of code as well

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,8 @@
-var generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector("#generate");
 
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  const passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
@@ -10,17 +10,21 @@ function writePassword() {
  
 generateBtn.addEventListener("click", writePassword);
 
+function generatePassword() {
+  let confirmLetter = window.confirm('Do you want letters in your password?');
+}
+
 // Assignment Code || ln 1
 
 // Write password to the #password input || ln 3-9
 
 // Add event listener to generate button || ln 11
 
-// Create generatePassword() function
+// Create generatePassword() function || ln 13
+
+// Confirm do you want to include letters? 
 
 // Confirm do you want to include numbers?
-
-// Confirm do you want to include Letters?
 
 // Confirm do you want to include special characters?
 
